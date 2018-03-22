97eb029961a316e2c032edc94698308d807e672c
shanmukhanand <s.barsagadey@gmail.com>
2018-03-22 22:14:25 +0530

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -4,7 +4,7 @@ package com.toolstest;
  * Hello world!
  */
 public class App {
-
+private String name;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }

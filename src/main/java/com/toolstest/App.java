804e611e049e3668654a37b121495521ab53320c
shanmukhanand <s.barsagadey@gmail.com>
2018-03-23 23:01:03 +0530

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -6,7 +6,13 @@ package com.toolstest;
 public class App {
 private String name;
     
+    private String id;
+    
     public static void main(String[] args) {
         System.out.println("Helfflo World!");
+    }
+    
+    public static void testing() {
+        System.out.println("Helfflo testing!");
     }
 }
